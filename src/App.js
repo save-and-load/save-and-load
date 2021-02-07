ca93da45a1041444f1f3fb9efd5912629fb755ba
Aria <appliedalpha@naver.com>
2021-02-08 02:02:46 +0900

    0.2 Testing Environment Variables

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and {process.env.REACT_APP_TEST}.
         </p>
         <a
           className="App-link"
