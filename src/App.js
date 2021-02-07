0fed8e07c6de615560f7e898923b44990e9c7606
Aria <appliedalpha@naver.com>
2021-02-08 02:16:58 +0900

    0.3 Temporary Page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and {process.env.REACT_APP_TEST}.
+          Save & Load 홈페이지가 될 곳입니다.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={process.env.REACT_APP_FOLDER}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          대학 밥 사기 목록 수정하러 가기
         </a>
       </header>
     </div>
