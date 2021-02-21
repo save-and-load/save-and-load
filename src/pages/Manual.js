03137c9eba82654e37aeca594b85f47d3aed2320
Aria <appliedalpha@naver.com>
2021-02-22 01:34:04 +0900

    0.7 Manual, Univs to Google Docs & IFrame

--- src/pages/Manual.js
+++ src/pages/Manual.js
@@ -1,38 +1,15 @@
-import React, { useState } from 'react';
-import { Document, Page, Outline, pdfjs } from 'react-pdf'
-import manualFile from "files/manualFile.pdf"
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React from 'react';
 
 const Manual = () => {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    const onDocumentLoadSuccess = ({numPages}) => {
-        setNumPages(numPages);
-    }
-
-    const prevPage = () => {
-        setPageNumber(num => Math.max(num - 1, 1));
-    }
-
-    const nextPage = () => {
-        setPageNumber(num => Math.min(num + 1, numPages));
-    }
-
     return (
         <div>
             <h2>법전</h2>
             <p>언젠가 보완할 듯?</p>
-            <p>Page {pageNumber} of {numPages}</p>
-            <button onClick={prevPage}>Prev Page</button>
-            <button onClick={nextPage}>Next Page</button>
-            <Document
-                file={manualFile}
-                onLoadSuccess={onDocumentLoadSuccess}
-                >
-                <Page pageNumber={pageNumber} scale={1.3} />
-            </Document>
+            <iframe src="https://docs.google.com/document/d/e/2PACX-1vRx68BvX4dmGzqE0bXthSyrtHHqGIJC29BRRX_Gy5OgJTycYUSgMyxdfbmiviP5F2uap63zAmEwNIkL/pub?embedded=true"
+                    width={window.innerWidth / 1.1}
+                    height={window.innerHeight / 1.5}
+                    title="법전"
+            />
         </div>
     );
 };
--- src/pages/Univs.js
+++ src/pages/Univs.js
@@ -1,38 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import {dbService} from "fBase";
-import Univ from "components/Univ.js";
-import UnivFactory from "components/UnivFactory";
+import React from 'react';
 
 const Univs = () => {
-    const [univList, setUnivList] = useState([]);
-
-    useEffect(() => {
-        dbService.collection("univ")
-            .orderBy("id", "asc")
-            .onSnapshot((snapshot) => {
-                const univArray = snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    ...doc.data(),
-                }))
-                setUnivList(univArray);
-            })
-    }, []);
-
-
     return (
         <div>
             <h2>대학 밥 사기 리스트 적는 곳</h2>
-            <a href={process.env.REACT_APP_FOLDER}>지금은 여기로..</a>
-            <div style={{ marginTop: 30 }}>
-                {univList.map(univ =>
-                    <Univ key={univ.id}
-                          univObj={univ}
-                    />
-                )}
-            </div>
-            {/*
-                <UnivFactory />
-            */}
+            {
+                /*
+                <a href={process.env.REACT_APP_FOLDER}>지금은 여기로..</a>
+                 */
+            }
+            <iframe
+                src="https://docs.google.com/spreadsheets/d/1z5-Ir6SVpyDSfC8bPVCu-8ngg1EgNHrz3ddZSOxxeLQ/edit?usp=sharing?gid=0&amp;range=A1:K20&amp;single=true&amp;widget=true&amp;headers=false"
+                width={window.innerWidth / 1.1}
+                height={window.innerHeight / 1.5}
+                title="대학 밥 사기 리스트 문서"
+            />
         </div>
     );
 };
